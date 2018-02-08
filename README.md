d1aba919bd39678dda95a3852f782a1263cbe799
PointMotion <pointmotion@Andrews-MacBook-Pro-4.local>
2018-02-08 17:17:15 -0500

    added readme and cleaned up

---
+++ README.md
@@ -0,0 +1,5 @@
+City Sounds Sample Player
+
+A very simple file playback app made with Objective-C and the csound API. Adjust the speed of the two samples with the slider. This one of my first attempts at programming.
+
+Andrew Perrin 2015
--- sampler/ViewController.m
+++ sampler/ViewController.m
@@ -10,17 +10,13 @@
 
 
 @interface ViewController () {
-
     IBOutlet UISlider *uiSlider;
     IBOutlet UILabel  *uiLabel;
-    
 }
 
 @end
 
 @implementation ViewController
-
-
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -42,33 +38,22 @@
 }
 
 - (IBAction)buttonTap:(UIButton *)sender {
-
-self.toggleButton.selected = !self.toggleButton.selected;
-   
+    self.toggleButton.selected = !self.toggleButton.selected;
     if (self.toggleButton.selected == YES){
-        
         NSString *india= [NSString stringWithFormat:@"i1 0 45\n"];
         [self.csound sendScore:india];
-        NSLog(@"bitchON");
         CsoundUI *csoundUI = [[CsoundUI alloc] initWithCsoundObj:self.csound];
         [csoundUI addLabel:uiLabel forChannelName:@"slider"];
         [csoundUI addSlider:uiSlider forChannelName:@"slider"];
-    
     }
-    
     else{
         NSString *indiastop = [NSString stringWithFormat:@"i99 0 0.3 1\n"];
         [self.csound sendScore:indiastop];
-        NSLog(@"bitchOFF");
-        //this will stop the csound file and stop playback
-        //       [self.csound stop];
-
     }
 }
 
 - (IBAction)londonButton:(UIButton *)sender {
     self.londonToggle.selected = !self.londonToggle.selected;
-    
     if (self.londonToggle.selected == YES){
         NSString *london = [NSString stringWithFormat:@"i2 0 45\n"];
         [self.csound sendScore:london];
@@ -76,16 +61,6 @@ self.toggleButton.selected = !self.toggleButton.selected;
     else {
         NSString *londonStop = [NSString stringWithFormat:@"i99 0 0.3 2"];
         [self.csound sendScore:londonStop];
-        
     }
-
 }
-
-
-
-
-
-
-
-
 @end
